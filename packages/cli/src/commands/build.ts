df55f4e1c9ddaa829696e5fa662a5d24f31cd071
Tony <tony.js@zoho.eu>
2022-11-30 19:07:31 -0300

    fix: bluish cli build command

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -6,27 +6,44 @@ import { Configuration } from '../models/Configuration'
 import { TriggerBuilderCollection } from '../models/TriggerBuilderCollection'
 import { glob } from '../tools/glob'
 import { _import } from '../tools/_import'
+import { getBluishConfiguration } from '../tools/getBluishConfiguration'
 
 interface BuildOptions {
   config: string
   input: string
   output: string
+  functions: string[]
+  app?: string
 }
 
 export const build = new Command('build')
 
-build.option('-c, --config <path>', '', 'bluish.config.ts')
-build.option('-i, --input <path>', '', (value) => path.resolve(value), process.cwd())
-build.option('-o, --output <path>', '', 'dist')
+build.option('-c, --config <path>', 'Bluish config file path')
+build.option(
+  '-i, --input <path>',
+  'Folder where the builder will mirror to create the triggers build',
+  (value) => path.resolve(value),
+  process.cwd(),
+)
+build.option('-o, --output <path>', 'Folder that will be the target of the triggers', 'dist')
+build.option<string[]>(
+  '-f, --functions <path>',
+  'glob of the files that will be compiled for triggers',
+  (functionGlob, functions) => functions.concat(functionGlob),
+  [],
+)
+build.option('-a, --app <path>', 'path of the file that will be loaded as a wrapper app')
 
 build.action(async () => {
   const opts = build.opts<BuildOptions>()
 
-  const configuration = await Configuration.from(
-    opts.input,
-    opts.config,
-    path.resolve(opts.input, opts.output),
-  )
+  const bluishConfiguration = await getBluishConfiguration(opts.input, opts.config)
+
+  if (opts.app) bluishConfiguration.application = opts.app
+
+  if (opts.functions.length) bluishConfiguration.functions = opts.functions
+
+  const configuration = new Configuration(opts, bluishConfiguration)
 
   if (await exists(configuration.output))
     await fs.rm(configuration.output, { force: true, recursive: true })
