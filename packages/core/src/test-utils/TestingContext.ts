962a8e6d4239f400e525329bdf6bcdbcae45e13d
Tony <tony.js@zoho.eu>
2022-12-14 07:12:13 -0300

    chore: remove unecessary tools for test

--- packages/core/src/test-utils/TestingContext.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from '../models/contexts/Context'
-
-export class TestingContext extends Context {
-  public handledError = (data: unknown) => this.success(data)
-  public success = (data: unknown) => data
-  public unhandledError = (error: unknown) => {
-    throw error
-  }
-}
--- packages/core/src/test-utils/TestingTrigger.ts
+++
@@ -1,6 +0,0 @@
-import { Trigger } from '../decorators/triggers/Trigger'
-import { TestingContext } from './TestingContext'
-
-export function TestingTrigger() {
-  return Trigger({ Context: TestingContext })
-}
